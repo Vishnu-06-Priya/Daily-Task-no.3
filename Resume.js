9a9329932f3b23c29a7cb15be07459ed412fbfdf
Vishnupriyadhanapal <115882330+Vishnu-06-Priya@users.noreply.github.com>
2022-12-01 00:56:49 -0800

    Add files via upload

---
+++ Resume.js
@@ -0,0 +1,54 @@
+[{
+    "basics": {
+    "name": "vishnupriya.d",
+    "email": "vishnupriyadhanapal6899@gmail.com",
+    "phone": "99976456434",
+    "degree": "bsc.,mathematics",
+    "dob":"1999-08-06",
+    "summary": "to be a successful professional in a globally respected company and to help the company to achieve the objective.",
+    "location": {
+    "address": "156,venkateshwara street",
+    "postalCode": "636 008",
+    "city": "salem",
+    "region": "tamilnadu"
+    },
+    },
+
+    
+    "education": [
+    {
+    "institution": "loyola college of arts and science",
+    "area": "aathur",
+    "studyType": "regular",
+    "startDate": "2016-06-28",
+    "endDate": "2019-05-09",
+    "gpa": "6.1",
+    "courses":"mathematics"
+    },
+    ],
+    "skills": [
+    {
+    "name": "typewritting,ms office,ppt",
+    }
+    ],
+    "languages": [
+    {
+    "language": "tamil,english",
+    "fluency": "read,write,speak"
+    }
+    ],
+    "interests": [
+    {
+    "name": "painting,designing,video eiting",
+    }
+    ],
+    
+    "curriculum vitae":[{
+        "name":"vishnupriya.d",
+        "gender":"female",
+        "mariatal status":"single",
+        "experience":"nil",
+        "expected salary":"8lpa",
+    },
+         
+    ]
